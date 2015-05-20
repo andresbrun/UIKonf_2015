9ac44532c126dd7b58aac06eb1eb974643dc966d
Romain Boulay <romain.boulay@wimdu.com>
2015-05-20 15:31:30 +0200

    Api client v2

--- FriendsGlue/FriendsGlue/APIClient.swift
+++ FriendsGlue/FriendsGlue/APIClient.swift
@@ -8,25 +8,37 @@
 
 import Foundation
 
+
 class APIClient {
-    func request(request: NSURLRequest, success: ((NSData, NSHTTPURLResponse?) -> Void)?, failure: ((NSError) -> Void)?) {
-        let basicAuthString = "APP_TOKEN" + ":" + "SESSION_TOKEN"
-        let basicAuthData = basicAuthString.dataUsingEncoding(NSUTF8StringEncoding)
-        let base64EncodedCredential = basicAuthData!.base64EncodedStringWithOptions(nil)
-        let authString = "Basic \(base64EncodedCredential)"
-        
-        let headers = [
-            "accept": "application/json",
-            "authorization": authString
+    let appToken = "eb3244b31222c1c6c13c9fb1cdd4e29d"
+    var sessionToken: String?
+    
+    
+    func requestSessionToken(success: (() -> Void), failure: ((NSError)? -> Void)?) {
+        let parameters = [
+            "user_name": "Demo User",
+            "first_name": "John",
+            "last_name": "Smith",
+            "email": "js@m.o",
+            "password": "password"
         ]
         
-        var request = NSMutableURLRequest(URL: NSURL(string: "https://api.tapglue.com/0.2/user")!,
-            cachePolicy: .UseProtocolCachePolicy,
-            timeoutInterval: 10.0)
-        
-        request.HTTPMethod = "GET"
-        request.allHTTPHeaderFields = headers
-        
+        let request = authenticatedMutableURLRequest("https://api.tapglue.com/0.2/users/create?withLogin=true", httpMethod: "POST")
+        let postData = NSJSONSerialization.dataWithJSONObject(parameters, options: nil, error: nil)
+        request.HTTPBody = postData
+
+        self.request(request, success: { [unowned self] (json, response) -> Void in
+            println(json)
+            
+            if let session = json["session_token"] as? String {
+                self.sessionToken = session
+            }
+            success()
+        }, failure: failure)
+    }
+    
+    
+    private func request(request: NSURLRequest, success: ((json: Dictionary<String, AnyObject>, response: NSHTTPURLResponse?) -> Void)?, failure: ((NSError)? -> Void)?) {
         let session = NSURLSession.sharedSession()
         let dataTask = session.dataTaskWithRequest(request, completionHandler: { (data, response, error) -> Void in
             if (error != nil) {
@@ -36,15 +48,52 @@ class APIClient {
                     failureBlock(error)
                 }
             } else {
-                let httpResponse = response as? NSHTTPURLResponse
-                println(httpResponse)
-                
-                if let successBlock = success {
-                    successBlock(data, httpResponse)
+                var parsedObject = Dictionary<String, AnyObject>()
+                if let httpResponse = response as? NSHTTPURLResponse {
+                    println(httpResponse)
+                    
+                    var parseError: NSError?
+                    parsedObject = NSJSONSerialization.JSONObjectWithData(data,
+                        options: NSJSONReadingOptions.AllowFragments,
+                        error:&parseError) as! Dictionary<String, AnyObject>
+                    
+                    if let successBlock = success {
+                        successBlock(json: parsedObject, response: httpResponse)
+                    }
+                }
+                else {
+                    if let failureBlock = failure {
+                        failureBlock(nil)
+                    }
                 }
             }
         })
         
         dataTask.resume()
+    }
+    
+    private func authenticatedMutableURLRequest(urlString: String, httpMethod: String = "GET") -> NSMutableURLRequest {
+        var basicAuthString = appToken + ":"
+        if let sessionTokenValue = sessionToken {
+            basicAuthString + sessionTokenValue
+        }
+
+        let basicAuthData = basicAuthString.dataUsingEncoding(NSUTF8StringEncoding)
+        let base64EncodedCredential = basicAuthData!.base64EncodedStringWithOptions(nil)
+        let authString = "Basic \(base64EncodedCredential)"
+        
+        let headers = [
+            "accept": "application/json",
+            "content-type": "application/json",
+            "authorization": authString
+        ]
+        
+        var request = NSMutableURLRequest(URL: NSURL(string: urlString)!,
+            cachePolicy: .UseProtocolCachePolicy,
+            timeoutInterval: 10.0)
+        
+        request.HTTPMethod = httpMethod
+        request.allHTTPHeaderFields = headers
+        return request
     }
 }
