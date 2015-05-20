a96f267435a9532b9bfb3674a9dca55533b11387
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-05-20 13:57:40 +0200

    Adding Map screen.

---
+++ FriendsGlue/FriendsGlue/EventCreatorViewController.swift
@@ -0,0 +1,13 @@
+//
+//  EventCreatorViewController.swift
+//  FriendsGlue
+//
+//  Created by Andres Brun Moreno on 20/05/15.
+//  Copyright (c) 2015 Andres Brun Moreno. All rights reserved.
+//
+
+import UIKit
+
+class EventCreatorViewController: UIViewController {
+
+}
