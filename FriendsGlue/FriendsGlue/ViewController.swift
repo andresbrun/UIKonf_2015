f7861fac9683d52c16b936143bdeb31df64cfd9e
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-05-20 11:06:58 +0200

    Initial commit.

---
+++ FriendsGlue/FriendsGlue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FriendsGlue
+//
+//  Created by Andres Brun Moreno on 20/05/15.
+//  Copyright (c) 2015 Andres Brun Moreno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
