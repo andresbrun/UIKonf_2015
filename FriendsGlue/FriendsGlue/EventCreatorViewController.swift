430dfdad863040fa656f3259e0e3e4af38289987
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-05-20 14:48:50 +0200

    What, When and Who almost working.

--- FriendsGlue/FriendsGlue/EventCreatorViewController.swift
+++ FriendsGlue/FriendsGlue/EventCreatorViewController.swift
@@ -8,6 +8,60 @@
 
 import UIKit
 
-class EventCreatorViewController: UIViewController {
+class EventCreatorViewController: UIViewController, UIActionSheetDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var whatTextField: UITextField!
+    @IBOutlet weak var whereTextfFeld: UITextField!
+    @IBOutlet weak var whenTextField: UITextField!
+    @IBOutlet weak var whoTextField: UITextField!
+    @IBOutlet weak var privacySegmented: UISegmentedControl!
+    
+    let actionTypes = ["Sport", "Beer", "Watch a Film", "Watch a Game"]
+    let datePicker = UIDatePicker()
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let whatActionSheet = UIActionSheet(title: "Choose Activity", delegate: self, cancelButtonTitle: nil, destructiveButtonTitle: nil, otherButtonTitles: "Sport", "Beer", "Watch a Film", "Watch a Game")
+        
+        whatTextField.inputView = whatActionSheet
+        
+        
+        whenTextField.inputView = datePicker
+        let toolbar = UIToolbar(frame: CGRectMake(0, 0, CGRectGetWidth(view.bounds), 44))
+        toolbar.setItems([UIBarButtonItem(barButtonSystemItem: .Done, target: self, action: "dateSelected:")], animated: false)
+        
+        whenTextField.inputAccessoryView = toolbar
+        
+    }
+    
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        
+        if let actionSheet = textField.inputView as? UIActionSheet {
+            actionSheet.showInView(self.view)
+        }
+        
+        if textField == whoTextField {
+            performSegueWithIdentifier("showFriendList", sender: self) 
+        }
+        
+        return false
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        if actionSheet == whatTextField.inputView {
+            whatTextField.text = actionTypes[buttonIndex]
+        }
+    }
+    
+    func dateSelected(sender: AnyObject) {
+        
+        let formatter = NSDateFormatter()
+        formatter.dateStyle = .ShortStyle
+        formatter.timeStyle = .MediumStyle
+
+        whenTextField.text = formatter.stringFromDate(datePicker.date)
+        whenTextField.resignFirstResponder()
+    }
 
 }
