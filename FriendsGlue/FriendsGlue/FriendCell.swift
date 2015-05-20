9808265b6c6bf67713dcdaee6a2ce12d7a09b394
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-05-20 12:06:55 +0200

    New Friend cell.

---
+++ FriendsGlue/FriendsGlue/FriendCell.swift
@@ -0,0 +1,15 @@
+//
+//  FriendCell.swift
+//  FriendsGlue
+//
+//  Created by Andres Brun Moreno on 20/05/15.
+//  Copyright (c) 2015 Andres Brun Moreno. All rights reserved.
+//
+
+import UIKit
+
+class FriendCell: UITableViewCell {
+    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var name: UILabel!
+
+}
--- FriendsGlue/FriendsGlue/FriendsViewController.swift
+++ FriendsGlue/FriendsGlue/FriendsViewController.swift
@@ -11,20 +11,16 @@ import UIKit
 class FriendsViewController: UITableViewController {
 
     let addressBook = APAddressBook()
+    var contactsRetrieved = [AnyObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
         
         addressBook.loadContacts(
             { (contacts: [AnyObject]!, error: NSError!) in
                 if let contactsValue = contacts {
-                    // do something with contacts
+                    self.contactsRetrieved = contactsValue
+                    self.tableView.reloadData()
                 }
         })
         
@@ -40,24 +36,25 @@ class FriendsViewController: UITableViewController {
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         // #warning Potentially incomplete method implementation.
         // Return the number of sections.
-        return 0
+        return 1
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete method implementation.
         // Return the number of rows in the section.
-        return 0
+        return contactsRetrieved.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as! UITableViewCell
+        let cell = tableView.dequeueReusableCellWithIdentifier("FriendCell", forIndexPath: indexPath) as! FriendCell
 
         // Configure the cell...
+        
 
         return cell
     }
-    */
+
 
     /*
     // Override to support conditional editing of the table view.
