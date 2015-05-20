6fd95def1158bec98572adf28fdaa5c8c4de856a
Andres Brun Moreno <andresbrunmoreno@gmail.com>
2015-05-20 16:24:23 +0200

    Mock data in event list + action sheet.

--- FriendsGlue/FriendsGlue/EventListViewController.swift
+++ FriendsGlue/FriendsGlue/EventListViewController.swift
@@ -8,19 +8,38 @@
 
 import UIKit
 
-class EventListViewController: UIViewController, UITableViewDataSource {
+class EventListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UIActionSheetDelegate {
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var tableView: UITableView!
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return segmentedControl.selectedSegmentIndex == 0 ? 4 : 20
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("EventTableViewCell") as! EventTableViewCell
         
         return cell
+    }
+    
+    func tableView(tableView: UITableView, shouldHighlightRowAtIndexPath indexPath: NSIndexPath) -> Bool {
+        
+        let actionSheet = UIActionSheet(title: "I want to...", delegate: self, cancelButtonTitle: nil, destructiveButtonTitle: "Meh, nope", otherButtonTitles: "Yeah, I am in!")
+        actionSheet.showInView(view)
+        
+        return false
+    }
+    
+    @IBAction func segmentControlChanged(sender: AnyObject) {
+        tableView.reloadData()
+    }
+    
+    func actionSheet(actionSheet: UIActionSheet, clickedButtonAtIndex buttonIndex: Int) {
+        
     }
 }
